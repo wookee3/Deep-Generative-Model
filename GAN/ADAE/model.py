5e3c83251efb82c19fea6310ef7b29073f59db7c
wookee3 <wookee329@gmail.com>
2019-05-03 19:04:12 +0900

    model start

--- GAN/ADAE/model.py
+++ GAN/ADAE/model.py
@@ -1,21 +1,71 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 from utils.loggings import logger
+from .layers import *
 
 
-def init_weight(m):
-    if type(m) == nn.Linear:
-        pass
-    elif type(m) == nn.Conv2d:
-        pass
-    elif type(m) == nn.Conv1d:
-        pass
-    else:
-        pass
+def init_weight(self):
+    def _init_weight(m):
+        if type(m) == nn.Linear:
+            pass
+        elif type(m) == nn.Conv2d:
+            pass
+        elif type(m) == nn.Conv1d:
+            pass
+        else:
+            pass
 
 
-class Encoder:
+class BaseModule(nn.Module):
     def __init__(self):
-        self.name = "enc"
+        super(BaseModule, self).__init__()
+
+    def forward(self, batch_data):
+        raise NotImplementedError()
+
+        
+class ImageEncoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, batch_data):
+        pass
+
+
+class LatentEncoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, batch_data):
+        pass
+
+
+class ImageDecoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, batch_data):
+        pass
+
+
+class LatentDiscriminator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, batch_data):
+        pass
+
+
+class ImageDiscriminator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, batch_data):
+        pass
+
+
+if __name__ == '__main__':
+    logger.info("test for mnist")
 
