7015f6f3e12a75f7e96afe0f0c1f49669b539d54
wookee3 <wookee329@gmail.com>
2019-05-06 21:01:47 +0900

    model

--- GAN/ADAE/layers.py
+++ GAN/ADAE/layers.py
@@ -9,17 +9,21 @@ def _single():
 
 
 # conv layer with SAME padding in tensorflow
+# TODO: padding == 'valid
 class Conv2d(nn.Module):
     def __init__(self, in_ch, out_ch, k_size, stride=1, dilation=1, padding='SAME'):
         super(Conv2d, self).__init__()
 
-        if isinstance(k_size, int):
-            pad_len = int((k_size - 1) * dilation / 2)
-            pad = nn.ConstantPad2d(pad_len, 0)
+        if padding.lower() == 'same':
+            if isinstance(k_size, int):
+                pad_len = int((k_size - 1) * dilation / 2)
+                pad = nn.ConstantPad2d(pad_len, 0)
+            else:
+                pad_len_h = int((k_size[0] - 1) * dilation / 2)
+                pad_len_w = int((k_size[1] - 1) * dilation / 2)
+                pad = nn.ConstantPad2d((pad_len_h, pad_len_h, pad_len_w, pad_len_w), 0)
         else:
-            pad_len_h = int((k_size[0] - 1) * dilation / 2)
-            pad_len_w = int((k_size[1] - 1) * dilation / 2)
-            pad = nn.ConstantPad2d((pad_len_h, pad_len_h, pad_len_w, pad_len_w), 0)
+            pass
         conv = nn.Conv2d(in_ch, out_ch, k_size, stride, dilation=dilation)
         self.sequence = nn.Sequential(pad, conv)
 
@@ -30,12 +34,15 @@ class Conv2d(nn.Module):
 class Conv1d(nn.Module):
     def __init__(self, in_ch, out_ch, k_size, stride=1, dilation=1, padding='SAME', causal=False):
         super(Conv1d, self).__init__()
-        if causal:
-            pad_len = (k_size - 1) * dilation
-            pad = nn.ConstantPad1d((pad_len, 0), 0)
+        if padding.lower() == 'same':
+            if causal:
+                pad_len = (k_size - 1) * dilation
+                pad = nn.ConstantPad1d((pad_len, 0), 0)
+            else:
+                pad_len = int((k_size-1)*dilation/2)
+                pad = nn.ConstantPad1d(pad_len, 0)
         else:
-            pad_len = int((k_size-1)*dilation/2)
-            pad = nn.ConstantPad1d(pad_len, 0)
+            pass    
         conv = nn.Conv1d(in_ch, out_ch, k_size, stride, dilation=dilation)
         self.sequence = nn.Sequential(pad, conv)
 
@@ -43,16 +50,21 @@ class Conv1d(nn.Module):
         return self.sequence(data)
         
 
+# TODO: causal deconv layers
 class Deconv2d(nn.Module):
     def __init__(self, in_ch, out_ch, k_size, stride=1, dilation=1, padding='SAME', out_padding=0):
         super(Deconv2d, self).__init__()
         assert type(k_size) == type(stride), 'k_size and stride should have same size'
-        if isinstance(k_size, int):
-            pad_len = int(((k_size - 1) * dilation + 1 - stride)/2)
+       
+        if padding.lower() == 'same':
+            if isinstance(k_size, int):
+                pad_len = int(((k_size - 1) * dilation + 1 - stride)/2)
+            else:
+                pad_len_h = int(((k_size[0] - 1) * dilation + 1 - stride[0])/2)
+                pad_len_w = int(((k_size[1] - 1) * dilation + 1 - stride[1]) / 2)
+                pad_len = (pad_len_h, pad_len_w)
         else:
-            pad_len_h = int(((k_size[0] - 1) * dilation + 1 - stride[0])/2)
-            pad_len_w = int(((k_size[1] - 1) * dilation + 1 - stride[1]) / 2)
-            pad_len = (pad_len_h, pad_len_w)
+            pass
         self.deconv = nn.ConvTranspose2d(in_ch, out_ch, k_size, stride, pad_len, dilation=dilation)
         
     def forward(self, data):
@@ -60,7 +72,7 @@ class Deconv2d(nn.Module):
         
 
 class Deconv1d(nn.Module):
-    def __init__(self, in_ch, out_ch, k_size, stride=1, dilation=1, padding='SAME', out_padding=0, causal=False):
+    def __init__(self, in_ch, out_ch, k_size, stride=1, dilation=1, padding='SAME', out_padding=0):
         super(Deconv1d, self).__init__()
         if causal:
             pass
@@ -84,7 +96,7 @@ if __name__ == '__main__':
     # print(a.size())
 
     # conv layer
-    conv2d = Conv2d(1, 3, 3, 1, 2)
+    conv2d = Conv2d(in_ch=1, out_ch=3, k_size=3, stride=1, dilation=2)
     conv1d = Conv1d(1, 3, 3, 1, 1, causal=False)
     def_conv2d = nn.Conv2d(1, 3, 3, 1, 2)
     
